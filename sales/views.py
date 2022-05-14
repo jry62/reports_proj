0b1803f9e7115a5aeeb4111dabed7bf572f76445
jry62 <102626853+jry62@users.noreply.github.com>
2022-05-14 09:47:29 -0400

    creating search forms

--- sales/views.py
+++ sales/views.py
@@ -2,11 +2,19 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
+
 # Create your views here.
 
 def home_view(request):
+    form = SalesSearchForm(request.POST or None)
     hello = 'hello world from the view'
-    return render(request, 'sales/home.html', {'hello': hello})
+    context = {
+        'hello': hello,
+        'form': form,
+
+    }
+    return render(request, 'sales/home.html', context)
 
 class SaleListView(ListView):
     model = Sale
@@ -27,3 +35,9 @@ def sale_detail_veiw(request, pk):
     # or 
     # obj = get_object_or_404(Sale, pk=pk)
     return render(request, 'sales/detail.html', {'object': obj})
+
+    ''''
+    in the urls:
+    path('sales/, sale_list_view, name = 'list'),
+    path('sales/<pk>/', sale_detail_view, name='detail'),
+    '''
