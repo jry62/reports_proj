797e034f9c56096e07c423c6151ad620b729fc77
jry62 <102626853+jry62@users.noreply.github.com>
2022-05-14 08:51:09 -0400

    working on the sales list

--- sales/views.py
+++ sales/views.py
@@ -1,7 +1,14 @@
+from typing import List
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Sale
 # Create your views here.
 
 def home_view(request):
     hello = 'hello world from the view'
-    return render(request, 'sales/main.html', {'hello': hello})
+    return render(request, 'sales/home.html', {'hello': hello})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
+    context_object_name = 'qs'
