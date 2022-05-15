80752dead2f7dc7b60fb2c586f8acce67995a909
jry62 <102626853+jry62@users.noreply.github.com>
2022-05-15 06:41:14 -0400

    Apply function

--- sales/utils.py
+++ sales/utils.py
@@ -1,6 +1,19 @@
 import uuid
+from flask import render_template_string
 
+from pandas import cut
+from customers.models import Customer
+from profiles.models import Profile
 
 def generate_code():
     code = str(uuid.uuid4()).replace('-', '').upper()[:12]
     return code
+
+
+def get_salesman_from_id(val):
+    salesman = Profile.objects.get(id=val)
+    return salesman.user.username
+
+def get_customer_from_id(val):
+    customer = Customer.objects.get(id=val)
+    return customer
