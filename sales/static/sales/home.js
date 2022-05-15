4f71c4a1d17a367a1a46d594db9a6194eb55c1ae
jry62 <102626853+jry62@users.noreply.github.com>
2022-05-15 12:05:21 -0400

    Create report Objects

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -3,9 +3,15 @@ console.log('hello world')
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
+const reportForm = document.getElementById('report-form')
 
-console.log(reportBtn)
-console.log(img)
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+console.log(reportName)
+console.log(reportRemarks)
+
+
 
 if (img){
     reportBtn.classList.remove('not-visible')
@@ -15,4 +21,30 @@ reportBtn.addEventListener('click', ()=>{
     console.log('clicked')
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+    
+    console.log(img.src)
+
+    reportForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf)
+        formData.append('name', reportName.value)
+        formData.append('remarks', reportRemarks.value)
+        formData.append('image', img.src)
+
+        $.ajax({
+            type: 'POST',
+            url: '/reports/save/',
+            data: formData,
+            success: function(response){
+                console.log(response)
+            },
+            error: function(error){
+                console.log(error)
+            },
+            processData: false,
+            contentType: false,
+
+        })
+    })
 })
